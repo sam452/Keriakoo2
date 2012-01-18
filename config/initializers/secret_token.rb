857728caf709e4372fd221eac0638fa40915fedb
sam walton <sam@foofrancine.com>
2012-01-18 13:35:52 -0600

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keriakoo::Application.config.secret_token = 'e6ac83a4dbf31db44d96c23571adaed10b1b120526ea4c3ef712bd662c469ffafbaacc5f67f8008ec096ff665409179ce16b7ff4bfad5353b893c4eab20646ec'
